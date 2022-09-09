0894947d370464e336f493656fc23098fc510c7d
zimitriy <zimitriy@mail.ru>
2022-09-09 19:33:02 +0300

    a2 done

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -1,58 +1,148 @@
 """
 Priority Queue
+
 Queue priorities are from 0 to 10
 """
 from typing import Any
 
 
+# class PriorityQueue:
+#     def __init__(self):
+#         self.pr_q = []
+#
+#     def enqueue(self, elem: Any, priority: int = 0) -> None:
+#         """
+#         Operation that add element to the end of the queue
+#
+#         :param elem: element to be added
+#         :return: Nothing
+#         """
+#
+#         item = {
+#             "elem": elem,
+#             "priority": priority
+#         }
+#
+#         if not self.pr_q:
+#             self.pr_q.append(item)
+#             return None
+#
+#         for index, current_item in enumerate(self.pr_q):
+#             if current_item["priority"] <= item["priority"]:
+#                 self.pr_q.insert(index, item)
+#                 break
+#         else:
+#             self.pr_q.append(item)
+#
+#     def dequeue(self) -> Any:
+#         """
+#         Return element from the beginning of the queue. Should return None if not elements.
+#
+#         :return: dequeued element
+#         """
+#
+#         if not self.pr_q:
+#             return None
+#
+#         return self.pr_q.pop()["elem"]
+#
+#     def peek(self, ind: int = 0, priority: int = 0) -> Any:
+#         """
+#         Allow you to see at the element in the queue without dequeuing it
+#
+#         :param ind: index of element (count from the beginning)
+#         :return: peeked element
+#         """
+#
+#         return self.pr_q[-1 - ind]["elem"]
+#
+#     def clear(self) -> None:
+#         """
+#         Clear my queue
+#
+#         :return: None
+#         """
+#         self.pr_q.clear()
+#
+#         return None
+
+
 class PriorityQueue:
     def __init__(self):
-        self.pr_q = []
+        self.pr_q = {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
+            6: [],
+            7: [],
+            8: [],
+            9: [],
+            10: [],
+        }
 
     def enqueue(self, elem: Any, priority: int = 0) -> None:
         """
         Operation that add element to the end of the queue
+
         :param elem: element to be added
         :return: Nothing
         """
-        item = {
-            "elem": elem,
-            "priority": priority
-        }
 
-        if not self.pr_q:
-            self.pr_q.append(item)
-            return None
-
-        for index, current_item in enumerate(self.pr_q):
-            if current_item["priority"] <= item["priority"]:
-                self.pr_q.insert(index, item)
-                break
-        else:
-            self.pr_q.append(item)
+        mini_q = self.pr_q[priority]
+        mini_q.append(elem)
 
     def dequeue(self) -> Any:
         """
         Return element from the beginning of the queue. Should return None if not elements.
+
         :return: dequeued element
         """
-        if not self.pr_q:
-            return None
-        return self.pr_q.pop()["elem"]
+
+        for i in range(len(self.pr_q)):
+            if self.pr_q[i]:
+                return self.pr_q[i].pop(0)
+
+        return None
 
     def peek(self, ind: int = 0, priority: int = 0) -> Any:
         """
         Allow you to see at the element in the queue without dequeuing it
+
         :param ind: index of element (count from the beginning)
         :return: peeked element
         """
-        return self.pr_q[-1 - ind]["elem"]
+
+        for i in range(len(self.pr_q)):
+            if self.pr_q[i]:
+                try:
+                    return self.pr_q[i][ind]
+                except IndexError:
+                    ind = 0
+
+        return None
 
     def clear(self) -> None:
         """
         Clear my queue
+
         :return: None
         """
-        self.pr_q.clear()
 
-        return None
+        self.pr_q = {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
+            6: [],
+            7: [],
+            8: [],
+            9: [],
+            10: [],
+        }
+
+        return
--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -6,4 +6,49 @@ def check_brackets(brackets_row: str) -> bool:
     :return: True if valid, False otherwise
     """
 
+    # 1 --------------
+
+    open_bracket = 0
+
+    for i in brackets_row:
+        if i == "(":
+            open_bracket += 1
+        elif i == ")":
+            open_bracket -= 1
+        if open_bracket < 0:
+            return False
+
+    return not open_bracket
+    if open_bracket == 0:
+        return True
     return False
+
+    # 2 -------------
+
+    # проходит # self.assertTrue(check_brackets("())(()"))
+    if brackets_row != '' and (brackets_row[0] == ')' or brackets_row[-1] == '('
+                               or brackets_row.count('(') != brackets_row.count(')')):
+        return False  # Уже проходит все тесты.
+    return True
+
+    # 3 -------------
+
+    brackets_open = "("
+    brackets_closed = ")"
+    stack = []  # стэк для входа-выхода скобок
+
+    for index, elem in enumerate(brackets_row):
+        if elem in brackets_open:
+            stack.append(elem)
+            # print(f"Вход: {len(stack)}")
+        if elem in brackets_closed:
+            # print(f"Выходят: {len(stack)}")
+            if len(stack) == 0:
+                return False
+            stack.pop()
+
+        # print(f"Прошли шаг - длина: {len(stack)}")
+        if index == len(brackets_row) - 1:
+            if len(stack) > 0:
+                return False
+    return True
